6e9b508f3c511a68d07835e9a26a804e54221c20
RedstoneScratch <31410740+redstone-scratch@users.noreply.github.com>
2023-08-13 21:04:57 -0400

    add setup code

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # DScratch
 a WIP port of Scratch to the Nintendo DS
+
+the interpreter is written to be as authentic as possible so that most Scratch projects will work within limitations
+
+<!--
+requires:
+- DSi or memory expansion
+the best way to run DScratch (at least for large projects) (and on real hardware) is in the 3DS's DSi mode
+-->
+
+will support:
+- most if not all blocks
+- project playing
+- project editing
+- export to `.sb3`
+
+# Installation
+<!--
+Go to the releases and download the latest `DScratch.nds`. Then put it anywhere on your SD card.
+-->
--- main.cpp
+++ main.cpp
@@ -1,10 +1,58 @@
 #include <nds.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#define SCREEN_TOP 0
+#define SCREEN_BOTTOM 1
+
+// u16 k_pressed;
+// u16 k_held;
+// u16 k_released;
+
+void Setup() {
+    // all setup code goes here
+    NF_Set2D(SCREEN_BOTTOM, 0);
+    NF_SetRootFolder("DSCRATCH_ROOT");
+    iprintf("dscratch\n");
+}
+
+void MainLoop() {
+    while(1) {
+        scanKeys();
+        // k_pressed = keysDown();
+        // k_held = keysHeld();
+        // k_released = keysUp();
+
+        if (keysDown() & KEY_START) break;
+
+        swiWaitForVBlank();
+    }
+}
+
+int ConfirmExit() {
+    iprintf("Press START  to confirm exit");
+    iprintf("Press B      to restart");
+    while (1) {
+        scanKeys();
+
+        if (keysDown() & KEY_START) break;
+
+        if (keysDown() & KEY_B) return 1;
+
+        swiWaitForVBlank();
+    }
+    return 0;
+}
 
 void main(void) {
-    iprintf("dscratch");
-
-    while(1) {
-        swiWaitForVBlank();
+    Setup();
+    MainLoop();
+    switch (ConfirmExit()) {
+        case 0:
+            exit(0);
+        case 1:
+            main(void);
+        default:
+            ConfirmExit();
     }
 }
